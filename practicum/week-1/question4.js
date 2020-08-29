6be82d2dfbd69cdaa5eac182a61f3feb518177e9
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-08-29 14:21:52 -0700

    Update question4.js

--- practicum/week-1/question4.js
+++ practicum/week-1/question4.js
@@ -14,7 +14,7 @@ var howManyCommon = function(A, B) {
 var count = 0;
 for (var i =0; i <= A.length; i++){
     for (var j =0; j <= B.length; j++){
-        if (A[i] === B[j]){
+        if (A[i] == B[j]){
             count ++;
         }
     }
