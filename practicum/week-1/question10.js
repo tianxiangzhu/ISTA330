57458718fcd44dfe336f218e01c3cfa9aaee2fda
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-09-01 11:13:29 -0700

    Update question10.js

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -7,5 +7,16 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+var str = n.toString();
+var min = parseInt(str[0]);
+var max = parseInt(str[0]);
+for (var i =1;i<str.length; i++){
+    if (max < parseInt(str[i])){
+        max = parseInt(str[i]);
+    }
+    else if (min > parseInt(str[i])){
+        min = parseInt(str[i]);
+    }
+}
+return (max-min)
 };
