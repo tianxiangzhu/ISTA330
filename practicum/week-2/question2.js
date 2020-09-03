be900ae800f71d16262f09e6e806da29f14e0593
tianxiangzhu <70231867+tianxiangzhu@users.noreply.github.com>
2020-09-03 10:38:05 -0700

    Update question2.js

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -9,7 +9,17 @@ Example: input: [-12,3,-1,5,-2,1,-7]
                  because [3,-1,5] has the largest sum.
           */
 
- var largestSubarray = function(input) {
-     
-    
- };
+var largestSubarray = function(input) {
+var max_sum = 0;  
+var max_ending = 0; 
+for (var i = 0; i < input.length; i++) { 
+        max_ending += input[i];
+         
+        if (max_sum < max_ending){ 
+        max_sum = max_ending;}
+
+        if (max_ending < 0){
+        max_ending = 0;} 
+} 
+return max_sum;
+};
