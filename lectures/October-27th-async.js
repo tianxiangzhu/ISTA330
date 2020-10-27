125645a267951c3bfc1cb69b9df7cb948712b646
habahram <hamid.bahramian@gmail.com>
2020-10-27 15:17:50 -0400

    .

--- lectures/October-27th-async.js
+++ lectures/October-27th-async.js
@@ -1,40 +1,25 @@
 var fs = require('fs');
 
-console.log('program started.');
-let wait = (ms) => {
-    var start = new Date().getTime();
-    var end = start;
-    while(end < start + ms) {
-      end = new Date().getTime();
-   }
- }
-let doSomething1 = (resolve, reject) => { 
-       
-      fs.readFile('test1.txt', (error, data) => {
-        if(error) {
-          reject(`something went wrong: ${error}`);
-        }
-        console.log('doSomething1 completed.');
-        resolve(data);
-      });                
-    
+console.log('Program started.');
+
+let doSomething = (data) => {
+    console.log(`number of the characters: ${data ? data.length : data}`);
 }
 
-let doSomething2 = (data) => {
-   console.log(`Number of characters in this file: ${data.length}`);
+let readFromTheFile = (resolve, reject) => {
+  fs.readFile('test1.txt', (error, data) => 
+  {
+     if(error) {
+       reject(`something went wrong: ${error}`);
+     }
+     console.log(`The file was read.`);     
+     resolve(data);
+  });
 }
-
-let p1 = new Promise((resolve, reject) => {
-  return doSomething1(resolve, reject);
-});
-
-p1.then(x => doSomething2(x));
-
-let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
-for(let i = 1; i < primes.length + 1; i++) {
-   console.log(`The ${i}${i == 2 ? 'nd' : (i == 3 ? 'rd' : 'th')} prime number is ${primes[i-1]}`);
-   wait(1000);
+let wrap = (resolve, reject) => {
+    readFromTheFile(resolve, reject);
 }
-console.log('Now I know what the first 15 prime numbers are!');
+new Promise(wrap).then(x => doSomething(x)).catch(e => console.log(e));
 
-console.log('program ended');
+console.log('Program finished.');
+
--- lectures/October-27th-sync.js
+++ lectures/October-27th-sync.js
@@ -1,38 +1,31 @@
 var fs = require('fs');
 
-console.log('program started.');
+console.log('Program started.');
 let wait = (ms) => {
-    var start = new Date().getTime();
-    var end = start;
-    while(end < start + ms) {
-      end = new Date().getTime();
-   }
+  var start = new Date().getTime();
+  var end = start;
+  while(end < start + ms) {
+    end = new Date().getTime();
  }
-let doSomething1 = () => {    
-       
-      fs.readFile('test1.txt', (error, data) => {
-        if(error) {
-          console.log(`something went wrong: ${error}`);
-        }
-        console.log(`doSomething1 completed.`);
-        return data;
-      }); 
-               
-    return 0;
+ console.log('The wait is over.');
 }
 
-let doSomething2 = (data) => {
-  console.log(`Number of characters in this file: ${data.length}`);
+let doSomething = (data) => {
+    console.log(`number of the characters: ${data ? data.length : data}`);
 }
 
-let result = doSomething1();
-doSomething2(result);
-
-let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
-for(let i = 1; i < primes.length + 1; i++) {
-   console.log(`The ${i}${i == 2 ? 'nd' : (i == 3 ? 'rd' : 'th')} prime number is ${primes[i-1]}`);
-   wait(1000);
+let readFromTheFile = () => {
+  fs.readFile('test1.txt', (error, data) => 
+  {
+     if(error) {
+       console.log(`something went wrong: ${error}`);
+     }
+     console.log(`The file was read.`);     
+     return data;
+  });
 }
-console.log('Now I know what the first 15 prime numbers are!');
+let result = readFromTheFile();
+doSomething(result);
 
-console.log('program ended');
+console.log('Program finished.');
+
